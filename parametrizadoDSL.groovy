4e0f76eb2e5a4ff7254be48121e789ba31fd6d57
xHavckedx <65540386+xHavckedx@users.noreply.github.com>
2023-02-13 19:30:03 +0100

    Eliminado los publishers

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -17,24 +17,4 @@ job('ejemplo2-job-DSL') {
   	steps {
     		shell("bash jobscript.sh")
     	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
-    	}
 }
